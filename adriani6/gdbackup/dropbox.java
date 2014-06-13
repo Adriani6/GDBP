f57eb3499d059eb1f370e40248d1e89b1e2c5df7
Adrian Iwaszkiewicz <omojpepe@gmail.com>
2014-06-13 13:52:10 +0100

    Create dropbox.java

---
+++ adriani6/gdbackup/dropbox.java
@@ -0,0 +1,42 @@
+import com.dropbox.core.*;
+import java.io.*;
+import java.util.Locale;
+
+public class DropBox {
+    
+    public static void main(String[] args) throws IOException, DbxException {
+        // Get your app key and secret from the Dropbox developers website.
+        final String APP_KEY = "INSERT_APP_KEY";
+        final String APP_SECRET = "INSERT_APP_SECRET";
+
+        DbxAppInfo appInfo = new DbxAppInfo(APP_KEY, APP_SECRET);
+
+        DbxRequestConfig config = new DbxRequestConfig("JavaTutorial/1.0",
+            Locale.getDefault().toString());
+        DbxWebAuthNoRedirect webAuth = new DbxWebAuthNoRedirect(config, appInfo);
+
+        // Have the user sign in and authorize your app.
+        String authorizeUrl = webAuth.start();
+        System.out.println("1. Go to: " + authorizeUrl);
+        System.out.println("2. Click \"Allow\" (you might have to log in first)");
+        System.out.println("3. Copy the authorization code.");
+        String code = new BufferedReader(new InputStreamReader(System.in)).readLine().trim();
+
+        // This will fail if the user enters an invalid authorization code.
+        DbxAuthFinish authFinish = webAuth.finish(code);
+        String accessToken = authFinish.accessToken;
+
+        DbxClient client = new DbxClient(config, accessToken);
+
+        System.out.println("Linked account: " + client.getAccountInfo().displayName);
+
+        File inputFile = new File("working-draft.txt");
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        try {
+            DbxEntry.File uploadedFile = client.uploadFile("/magnum-opus.txt",
+                DbxWriteMode.add(), inputFile.length(), inputStream);
+            System.out.println("Uploaded: " + uploadedFile.toString());
+        } finally {
+            inputStream.close();
+        }
+}
